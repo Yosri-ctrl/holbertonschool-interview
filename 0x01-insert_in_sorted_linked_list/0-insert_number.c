c606fb61751e9850be0e2aea1d9296c9e518274e
Yosri-ctrl <yosribouabid@gmail.com>
2020-12-16 20:22:21 +0100

    sorted inert

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert a node in a sorted list
+ * @head: pointer to head of list
+ * @number: the nbr to insert in the list
+ * Return: number of nodes
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *pre, *new;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+    new->n = number;
+
+    pre = *head;
+
+    if (pre->n > new->n)
+    {
+        new->next = pre;
+        *head = new;
+        return (*head);
+    }
+
+    
+    while (pre != NULL)
+    {
+        if (pre->next->n > new->n)
+        {
+            new->next = pre->next;
+            pre->next = new;
+            
+            return (new);
+        }
+        pre = pre->next;
+    }
+    pre->next = new;
+    new->next = NULL;
+    return (new);
+}
