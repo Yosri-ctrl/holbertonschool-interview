0dfc8e8f6b184936cb23968156ec500bf0ea9951
Yosri-ctrl <yosribouabid@gmail.com>
2020-12-22 12:42:55 +0100

    task 0

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,14 +11,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new;
 
-    new = malloc(binary_tree_t);
+    new = malloc(sizeof(binary_tree_t));
     if (!new)
-        return (NULL)
-    
+        return (NULL);
+    /*
     if (parent->n > value)
         parent->left = new;
     else
-        parent->right = new;
+        parent->right = new;*/
 
     new->n = value;
     new->parent = parent;
