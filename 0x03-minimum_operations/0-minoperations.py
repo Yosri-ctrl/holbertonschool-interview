dcbf2a3fa32d597ebfd40cca5fc4a5f8f99ba17e
Yosri-ctrl <yosribouabid@gmail.com>
2021-01-07 13:38:15 +0100

    task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,11 +5,11 @@
 def minOperations(n):
     x = 1
     c = 0
-    if n == 0:
+    if n == 0 or n == 1:
         return 0
     while x < n:
-        if ((3 * x) % n) == 0:
-            x += 2 * x
+        if (x*2) < n and (n % (x*2)) == 0:
+	        x *= 2
             c += 2
         else:
             x += x/2
