06bfe5c993d7a2db5483be9ae27a52b7c97d6091
Yosri-ctrl <yosribouabid@gmail.com>
2021-01-27 21:25:18 +0100

    fixing format

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,14 +6,17 @@ code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 count = 0
 for _ in range(10000):
     try:
-        for _ in range(10):
+        for i in range(10):
             data = sys.stdin.readline()
+            #print(data)
             part = data.split()
+            #print(part)
             count += int(part[-1])
             code[int(part[-2])] += 1
     except Exception:
         pass
-    print("File size: {}".format(count))
-    for i in code:
-        if code[i] != 0:
-            print("{}: {}".format(i, code[i]))
+    finally:
+        print("File size: {}".format(count))
+        for i in code:
+            if code[i] != 0:
+                print("{}: {}".format(i, code[i]))
