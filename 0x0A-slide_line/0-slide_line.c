29c7662d9c53f5194c0ff08473c4da7970966352
Yurei <yosribouabid@gmail.com>
2021-02-25 11:08:30 +0100

    task 0

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -5,7 +5,7 @@
  * @line: points to an array of integers containing
  * @size: elements, that must be slided & merged to the
  * @direction: represented by direction
- * Return: 1 upon success, or 0 upon failure.
+ * Return: 1 in success, or 0 when failure.
  */
 int slide_line(int *line, size_t size, int direction)
 {
@@ -17,12 +17,9 @@ int slide_line(int *line, size_t size, int direction)
             new[i] = line[i];
         else
             new[i] = 0;
-        //printf("new[%ld] = %d\n", i, new[i]);
     }
-
     for (i = 0; i < size; i++)
     {
-        //printf("new[%ld] = %d\n", i, new[i]);
         for (j = i + 1; j < size; j++)
         {
             if (new[i] != 0 && new[j] != 0 && new[i] != new[j])
@@ -35,16 +32,6 @@ int slide_line(int *line, size_t size, int direction)
             }
         }
     }
-    /*
-    for (i = 0, j = 0; i < size; i++)
-    {
-        if (new[i] != 0)
-        {
-            new[j] = new[i];
-            new[i] = 0;
-            j++;
-        }
-    }*/
     for (i = 0; i < size; i++)
         printf("new[%ld] = %d\n", i, new[i]);
     for (i = 0; i < size; i++)
@@ -63,7 +50,6 @@ int slide_line(int *line, size_t size, int direction)
             line[j] = 0;
         break;
     case 1:
-        //printf("1:%ld,%ld\n", i, j);
         for (j = 0, i = 0; j < size; j++)
             if (new[j] == 0)
             {
