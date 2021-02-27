27bb5610fb1733e64d6cc6b3f47bf31fe0d88d6c
Yurei <yosribouabid@gmail.com>
2021-02-27 12:14:02 +0100

    header

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -9,5 +9,8 @@
 #define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
+void addition(int *line, size_t size);
+void alignement(int *line, size_t size);
+void reverse(int *line, size_t size);
 
 #endif
