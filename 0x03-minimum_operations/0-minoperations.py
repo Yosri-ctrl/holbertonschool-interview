9723129fcc1d0c24464cf54827dc318bdfab4e4d
Yurei <yosribouabid@gmail.com>
2021-03-09 11:32:30 +0100

    fix loop

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,16 +3,13 @@
 
 
 def minOperations(n):
-    x = 1
+    x = 2
     c = 0
     if n == 0 or n == 1:
         return (0)
-    while x < n:
-        if (x*2) < n and (n % (x*2)) == 0:
-            x *= 2
-            c += 2
-        else:
-            x += x/2
-            c += 1
-
+    while n > 1:
+        while n % x == 0:
+            c += x
+            n /= x
+        x += 1
     return (c)
