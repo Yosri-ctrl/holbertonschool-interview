70c74d296ee77133fdde4446d5678ab10063cfd0
Yurei <yosribouabid@gmail.com>
2021-03-13 14:46:16 +0100

    pep8 fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,27 +9,27 @@ n = 0
 try:
     for line in sys.stdin:
         if n != 0 and n % 10 == 0:
-            print("File size: {:d}".format(count))
+            print("File size: {}".format(count))
             for stat, times in code.items():
-                if times != 0:
-                    print("{:d}: {:d}".format(stat, times))
+                if times:
+                    print("{}: {}".format(stat, times))
         n += 1
         data = line.split()
         try:
-            stat = int(data[-2])  
-            #print("s={:d}".format(stat))
+            stat = int(data[-2])
+            # print("s={:d}".format(stat))
             if stat in code:
                 code[stat] += 1
             count += int(data[-1])
         except:
             pass
-    print("File size: {:d}".format(count))
+    print("File size: {}".format(count))
     for stat, times in code.items():
-        if times != 0:
-            print("{:d}: {:d}".format(stat, times))
+        if times:
+            print("{}: {}".format(stat, times))
 except KeyboardInterrupt:
-    print("File size: {:d}".format(count))
+    print("File size: {}".format(count))
     for stat, times in code.items():
-        if times != 0:
-            print("{:d}: {:d}".format(stat, times))
+        if times:
+            print("{}: {}".format(stat, times))
     raise
