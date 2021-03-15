90bde3d4809df22cc1d9e6cb69df57eca0e856b4
Yurei <yosribouabid@gmail.com>
2021-03-15 12:11:18 +0100

    fixing loops

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,35 +4,28 @@ import sys
 
 
 if __name__ == "__main__":
-    """stats"""
-    code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    
+    status = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
     count = 0
     n = 0
 
     try:
         for line in sys.stdin:
-            if n != 0 and n % 10 == 0:
-                print("File size: {}".format(count))
-                for stat, times in code.items():
-                    if times:
-                        print("{}: {}".format(stat, times))
             n += 1
-            data = line.split()
-            try:
-                stat = int(data[-2])
-                # print("s={:d}".format(stat))
-                if stat in code:
-                    code[stat] += 1
-                count += int(data[-1])
-            except:
-                pass
-        print("File size: {}".format(count))
-        for stat, times in code.items():
-            if times:
-                print("{}: {}".format(stat, times))
+            inp = line.split(' ')
+            if len(inp) > 2:
+                count += int(inp[-1])
+                if inp[-2] in status:
+                    status[inp[-2]] += 1
+            if n % 10 == 0:
+                print("File size: {}".format(count))
+                for stat in status.keys():
+                    if status[stat]:
+                        print("{}: {}".format(stat, status[stat]))
     except KeyboardInterrupt:
-        print("File size: {}".format(count))
-        for stat, times in code.items():
-            if times:
-                print("{}: {}".format(stat, times))
-        raise
+        pass
+    print("File size: {}".format(count))
+    for stat in status.keys():
+        if status[stat]:
+            print("{}: {}".format(stat, status[stat]))
