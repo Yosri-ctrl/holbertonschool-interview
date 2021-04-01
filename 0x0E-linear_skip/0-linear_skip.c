3aaf66dab983783e4ccdc55d979cdf6ca2b8c33e
Yurei <yosribouabid@gmail.com>
2021-04-01 19:21:59 +0100

    task 0

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,45 @@
+#include "search.h"
+/**
+ * linear_skip - search for vlue in the skip-list
+ * @list: the list to search in
+ * @value: the value to look for
+ * Return: the node of the value
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node = list, *prev = list, *next = list->express;
+
+	if (list == NULL)
+		return (NULL);
+	while (next)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", next->index, next->n);
+		if (next->n >= value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				   node->index, next->index);
+			break;
+		}
+		node = node->express;
+		next = next->express;
+	}
+	if (node->express == NULL)
+	{
+		prev = node;
+		while (prev->next)
+			prev = prev->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   node->index, prev->index);
+	}
+	prev = node;
+	while (prev != node->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			break;
+		prev = prev->next;
+	}
+	if (prev != node->express)
+		return (prev);
+	return (NULL);
+}
