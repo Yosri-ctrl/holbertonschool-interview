93e9069a8f0ba798f457da13d3f3ce2c9505f02d
Yurei <yosribouabid@gmail.com>
2021-04-06 20:54:33 +0100

    fix order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
 let request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/'
+if (!process.argv[2]){
+	return;
+}
+let id = process.argv[2];
+let url = `https://swapi-api.hbtn.io/api/films/${id}/`;
 request(url, function(err, res, body){
-	let id = process.argv.slice(2)[0];
 	let json = JSON.parse(body);
-	for (let i = 0; i < json["count"]; i++) {
-		if (json["results"][i]["episode_id"] == id){
-			let char_len = json["results"][i]["characters"].length
-			for (let j = 0; j < char_len - 1; j++) {
-				let char_url = json["results"][i]["characters"][j];
-				request(char_url, function(err, res, body){
-					let char_json = JSON.parse(body);
-					console.log(char_json["name"]);
-				});
-			}
-		}
+	//console.log(json);
+	//let char_len = json["results"][i]["characters"].length
+	for (let char of json.characters) {
+		request(char, function(err, res, body){
+			console.log(JSON.parse(body).name);
+		});
 	}
 })
