cebaefdd5c08f250c53e6e07f990001009ff4214
Yurei <yosribouabid@gmail.com>
2021-04-14 14:58:49 +0100

    pep8 fix

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,15 +7,15 @@ def rain(list):
     list: is the list to treat
     """
     count = 0
-    for i in range(1, len(list) - 1) :
+    for i in range(1, len(list) - 1):
         left = list[i]
-        for j in range(i) :
+        for j in range(i):
             left = max(left, list[j])
-        
-        right = list[i] 
-        for k in range(i + 1 , len(list)) :
+
+        right = list[i]
+        for k in range(i + 1, len(list)):
             right = max(right, list[k])
 
         count += (min(left, right) - list[i])
- 
+
     return count
