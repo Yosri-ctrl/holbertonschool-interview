7a7d5bfd9fbb768e4b3f846e04425b5fda319ef3
Yurei <yosribouabid@gmail.com>
2021-06-06 22:19:24 +0100

    fix n*n matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,12 +6,9 @@
 def rotate_2d_matrix(matrix):
   """ rotate 2d matrix 
   """
-  for i in range(len(matrix)):
-    for j in range(i,len(matrix)):
-      matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
-  
-  for i in range (len(matrix)):
-    k = len(matrix) - 1
-    for j in range(0,k):
-      matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
-      k -= 1
+  copy = matrix.copy()
+  for col in range(len(matrix[0])):
+    tmp = []
+    for row in reversed(copy):
+      tmp.append(row[col])
+      matrix[col] = tmp
