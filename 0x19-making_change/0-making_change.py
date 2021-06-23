0fc6ba3bd128276b9a4594cf65cf574addfc197c
Yurei <yosribouabid@gmail.com>
2021-06-23 13:55:43 +0100

    documentation

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""
+""" determine the fewest number of coins needed to meet total
 """
 
 
 def makeChange(coins, total):
+  """return the fewest number of coins needed to meet a given amount
+  """
     if total <= 0:
         return -1
     count = 0
