024cd830202ad89045029a08f1e753e40e503a5e
Yurei <yosribouabid@gmail.com>
2021-06-23 13:59:26 +0100

    pucodstyle fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,8 +4,8 @@
 
 
 def makeChange(coins, total):
-  """return the fewest number of coins needed to meet a given amount
-  """
+    """return the fewest number of coins needed to meet a given amount
+    """
     if total <= 0:
         return -1
     count = 0
@@ -13,8 +13,8 @@ def makeChange(coins, total):
         count += int(total / max(coins))
         total -= max(coins) * count
         pre = coins.pop(coins.index(max(coins)))
-
-        if total <= 0: break
-
-    if total + pre != 0: return -1
+        if total <= 0:
+            break
+    if total + pre != 0:
+        return -1
     return (count)
